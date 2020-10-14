4cfd1043e45cf2ae70c2bf3a0a244776b11c4b42
shane <shane@DESKTOP-M0LRK0P>
2020-10-14 10:09:42 +0100

    Thermometer

---
+++ Test/src/lab1/ThermTest.java
@@ -0,0 +1,22 @@
+package lab1;
+
+import lab1.Thermometer;
+
+//Student Name 	: Oisin Cawley
+//Student Id Number: 
+//Date 			: Nov-2015
+//Purpose 			: My first class implementation
+
+public class ThermTest
+{ // begin class ThermTest
+	public static void main(String args[]) 
+	{ // being main method
+
+		Thermometer thermA = new Thermometer();		// Create an instance of our Thermometer class
+
+		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		thermA.setCelsius(20.0);
+		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		
+	} // end main
+} // end cl
