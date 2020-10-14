3608b1f57702355dfbecf743337ec7de44c31b72
shane <shane@DESKTOP-M0LRK0P>
2020-10-14 11:14:33 +0100

    Added roomC

--- Test/src/lab2/HotelDriver.java
+++ Test/src/lab2/HotelDriver.java
@@ -9,8 +9,14 @@ public class HotelDriver {
 		HotelRoom roomB=new HotelRoom(201, "Double");
 		System.out.println("Room number is "+roomA.getRoomNumber()+", type is "+roomA.getRoomType());		
 		System.out.println("Room number is "+roomB.getRoomNumber()+", type is "+roomB.getRoomType());
+		roomA.setVacant(1);
+		roomA.setRate(100.0);
+		roomB.setVacant(0);
+		roomB.setRate(80.0);
 		System.out.println(roomA.toString());
 		System.out.println(roomB.toString());
+		HotelRoom roomC=new HotelRoom(202,"Single",0,90.0);
+		System.out.println(roomC.toString());
 	}
 
 }
--- Test/src/lab2/HotelRoom.java
+++ Test/src/lab2/HotelRoom.java
@@ -4,17 +4,25 @@ public class HotelRoom {
 	
 private int roomNumber;
 private String roomType;
-
-
+private int vacant;
+private double rate;
 
 public HotelRoom(int roomNumber, String roomType) {
 	setRoomNumber(roomNumber);
 	setRoomType(roomType);
 }
+public HotelRoom(int roomNumber, String roomType, int vacant, double rate) {
+	setRoomNumber(roomNumber);
+	setRoomType(roomType);
+	setVacant(vacant);
+	setRate(rate);
+}
 
 public HotelRoom() {
 	setRoomNumber(0);
 	setRoomType("");
+	setVacant(0);
+	setRate(0.0);
 }
 
 public int getRoomNumber() {
@@ -30,9 +38,28 @@ public void setRoomType(String roomType) {
 	this.roomType = roomType;
 }
 
+public int getVacant() {
+	return vacant;
+}
+
+public void setVacant(int vacant) {
+	this.vacant = vacant;
+}
+
+public double getRate() {
+	return rate;
+}
+
+public void setRate(double rate) {
+	this.rate = rate;
+}
+
 @Override
 public String toString() {
-	return "HotelRoom [roomNumber=" + roomNumber + ", roomType=" + roomType + "]";
+	return "HotelRoom [roomNumber=" + roomNumber + ", roomType=" + roomType + ", vacant=" + vacant + ", rate=" + rate
+			+ "]";
 }
+
+
 
 }
