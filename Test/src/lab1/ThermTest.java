f22ed0a9968a3fde60624a049ab89d30b588d979
shane <shane@DESKTOP-M0LRK0P>
2020-10-14 10:18:58 +0100

    Finished lab

--- Test/src/lab1/ThermTest.java
+++ Test/src/lab1/ThermTest.java
@@ -15,6 +15,9 @@ public class ThermTest
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
 		thermA.setCelsius(20.0);
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		Thermometer thermB=new Thermometer(10.0);
+		double tempB=thermB.getCelsius();
+		System.out.println("Temp. of Thermometer B is "+tempB);
 		
 	} // end main
 } // end cl
