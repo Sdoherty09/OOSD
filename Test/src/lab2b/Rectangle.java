41a69c79c460b59e8b98c76861bbb42d860b8347
shane <shane@DESKTOP-M0LRK0P>
2020-10-14 23:11:52 +0100

    Prints rectangle

--- Test/src/lab2b/Rectangle.java
+++ Test/src/lab2b/Rectangle.java
@@ -40,6 +40,41 @@ public void setWidth(double width) {
 		System.out.println("Invalid length");
 	}
 }
+public double getArea()
+{
+	return width*length;
+}
+public double getPerimeter()
+{
+	return (width*2)+(length*2);
+}
+public String printRectangle()
+{
+	String output="";
+	for (int i=0;i<width;i++)
+	{
+		for(int j=0;j<length;j++)
+		{
+			if(i==0||i==width-1)
+			{
+				output+="*";
+			}
+			else
+			{
+				if(j==0||j==length-1)
+				{
+					output+="*";
+				}
+				else
+				{
+					output+=" ";
+				}
+			}
+		}
+		output+="\n";
+	}
+	return output;
+}
 @Override
 public String toString() {
 	return "Rectangle [length=" + length + ", width=" + width + "]";
--- Test/src/lab2b/RectangleDriver.java
+++ Test/src/lab2b/RectangleDriver.java
@@ -5,8 +5,10 @@ public class RectangleDriver {
 	public static void main(String[] args) {
 		Rectangle rect=new Rectangle();
 		System.out.println(rect.toString());
-		rect=new Rectangle(5,10);
+		rect=new Rectangle(10,4);
 		System.out.println(rect.toString());
+		System.out.println("Area: "+rect.getArea()+"\nPerimeter: "+rect.getPerimeter());
+		System.out.println(rect.printRectangle());
 	}
 	
 }
