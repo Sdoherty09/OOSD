70aceca099f79daffe976ee71cef8bda50624a67
shane <shane@DESKTOP-M0LRK0P>
2020-11-25 11:36:29 +0000

    Added triangle

--- Test/src/lab8/Driver.java
+++ Test/src/lab8/Driver.java
@@ -10,13 +10,14 @@ public class Driver
   		Rectangle	rectangle = new Rectangle("Rectangle One", "Yellow", 15.0, 20.0);
   		Cylinder	cylinder = new Cylinder("Cylinder One", "Green", 6, 8);
   		Sphere		sphere = new Sphere("Sphere One", "Blue", 77);
+  		Triangle    triangle = new Triangle("Triangle One", "Orange", 9, 4);
   		
   		//print them using references of the objects type
   		System.out.println("\n---Using circle reference" + circle.toString());
   		System.out.println("\n---Using rectangle reference" + rectangle.toString());
   		System.out.println("\n---Using cylinder reference" + cylinder.toString());
   		System.out.println("\n---Using sphere reference" + sphere.toString());
-  		
+  		System.out.println("\n---Using triangle reference" + triangle.toString());
   		
 		//		This will not work as a circle or a rectangle are not ThreeDShape objects
 		//  	threeDShape = circle;   
@@ -33,9 +34,14 @@ public class Driver
   		System.out.println("\n---Using shape superclass reference for a rectangle " + shape.toString());
   		System.out.println("Rectangle area = " + shape.area()); //polymorphic call using superclass reference
   		
+  		shape = triangle;
+  		System.out.println("\n---Using shape superclass reference for a triangle " + shape.toString());
+  		System.out.println("Triangle area = " + shape.area());  //polymorphic call using superclass reference
+  		
   		shape = cylinder;
   		System.out.println("\n---Using shape superclass reference for a cylinder " + shape.toString());
   		System.out.println("Cylinder area = " + shape.area());  //polymorphic call using superclass reference
+  		
   		
   		threeDShape = cylinder;
   		System.out.println("\n---Using threeDShape superclass reference for a cylinder " + threeDShape.toString());
