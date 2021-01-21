b76107a7921c5d015efc6c77fe68b85c628be7bb
shane <shane@DESKTOP-M0LRK0P>
2021-01-21 13:43:18 +0000

    Added red, blue, green

---
+++ Test/src/lab10/MyColoursDriver.java
@@ -0,0 +1,16 @@
+package lab10;
+
+import javax.swing.JFrame;
+
+public class MyColoursDriver {
+
+	public static void main(String[] args) {
+	
+		MyColours changeColour = new MyColours("Button");
+		changeColour.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		changeColour.setSize(200, 200);
+		changeColour.setVisible(true);
+
+	}//end main
+
+}//end class
